4b7fe96a6977daf4d87a9ec80ff37550727ee51c
Huascar <huascarmc@hotmail.com>
2017-10-31 13:03:21 +0000

    added capacity to classroom

--- Classroom.java
+++ Classroom.java
@@ -1,3 +1,7 @@
 class Classroom {
- 
+ private Person[] students;
+
+ public Classroom(int capacity) {
+  this.students = new Person[capacity];
+ }
 }
--- ClassroomTest.java
+++ ClassroomTest.java
@@ -3,5 +3,16 @@ import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
 public class Classroom {
- 
+
+ Classroom classroom;
+
+ @Before
+ public void before(){
+  this.classroom = new Classroom(20);
+ }
+
+ @Test
+ public void testClassroomSetup(){
+  assertEquals( 20, classroom.length() )
+ }
 }
